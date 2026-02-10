2d121be83d327d88bfa6d4cb479d8816e3d64c55
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-02-10 12:25:09 -0300

    Add sidebar navigation structure to sidebar.js

--- portal/js/sidebar.js
+++ portal/js/sidebar.js
@@ -1,1 +1,28 @@
+const sidebar = document.getElementById('sidebar');
+
+sidebar.innerHTML = `
+  <nav>
+    <ul>
+      <li data-page="inicio">🏠 Início</li>
+
+      <li class="group">
+        👥 Gestão de Equipe
+        <ul>
+          <li data-page="equipe/gestao">Gestão</li>
+        </ul>
+      </li>
+
+      <li class="group">
+        📄 Solicitações
+        <ul>
+          <li data-page="solicitacoes/lista">Lista</li>
+          <li data-page="solicitacoes/nova">Nova</li>
+        </ul>
+      </li>
+
+      <li data-page="escala/index">📅 Escala</li>
+      <li data-page="materiais/index">📦 Materiais</li>
+    </ul>
+  </nav>
+`;
 
