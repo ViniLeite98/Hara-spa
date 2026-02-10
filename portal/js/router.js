e51816fcd2124c0b610446a001f57b1e6a1ad267
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-02-10 12:25:25 -0300

    Update router.js

--- portal/js/router.js
+++ portal/js/router.js
@@ -1,1 +1,17 @@
 
+const content = document.getElementById('content');
+
+function loadPage(page) {
+  fetch(`pages/${page}.html`)
+    .then(res => res.text())
+    .then(html => content.innerHTML = html);
+}
+
+document.addEventListener('click', e => {
+  if (e.target.dataset.page) {
+    loadPage(e.target.dataset.page);
+  }
+});
+
+// página inicial
+loadPage('inicio');
